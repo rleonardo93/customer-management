dbb27e92634894ae8009ca9dc1b70a063262cf0c
Rúben Leonardo <rleonardo.pt@gmail.com>
2020-05-10 18:25:25 +0100

    Instantiate a Spring application context and configure Spring MVC

---
+++ src/main/java/com/multicert/customermanagement/config/AppConfig.java
@@ -0,0 +1,39 @@
+package com.multicert.customermanagement.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScans(value = {
+        @ComponentScan("com.multicert.customermanagement.dao"),
+        @ComponentScan("com.multicert.customermanagement.service")})
+public class AppConfig {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Bean
+    public LocalSessionFactoryBean getSessionFactory() {
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+        factoryBean.setConfigLocation(context.getResource("classpath:hibernate.cfg.xml"));
+        factoryBean.setPackagesToScan("com.multicert.customermanagement.model");
+
+        return factoryBean;
+    }
+
+    @Bean
+    public HibernateTransactionManager getTransactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(getSessionFactory().getObject());
+
+        return transactionManager;
+    }
+}
---
+++ src/main/java/com/multicert/customermanagement/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.multicert.customermanagement.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "com.multicert.customermanagement.controller" })
+public class WebConfig implements WebMvcConfigurer {
+
+}
