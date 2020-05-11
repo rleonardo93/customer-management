8c6edc2f8c273f619a46a47462830ee10675515e
Rúben Leonardo <rleonardo.pt@gmail.com>
2020-05-11 15:55:30 +0100

    Add CustomerControllerTest for unit test against CustomerController

---
+++ src/test/java/com/multicert/customermanagement/controller/CustomerControllerTest.java
@@ -0,0 +1,40 @@
+/*
+package com.multicert.customermanagement.controller;
+
+import com.multicert.customermanagement.config.AppConfig;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { AppConfig.class })
+@WebAppConfiguration
+public class CustomerControllerTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+    private MockRestServiceServer mockRestService;
+
+
+    @Before
+    public void setUp() throws Exception {
+        // Provide support for Spring MVC testing.
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}*/
